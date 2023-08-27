7bd1ab0726f55ae25460d2346d43d48f453eada7
PiotrekB416 <piotrek15.pb@gmail.com>
2023-08-27 22:10:26 +0200

    updates

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -14,7 +14,7 @@ return require('packer').startup(function(use)
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
-
+  use 'Exafunction/codeium.vim'
 --  use({
 --	  'rose-pine/neovim',
 --	  as = 'rose-pine',
--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -2,6 +2,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>n", vim.cmd.Neotree)
 
+
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
